f7e93f371bb59d3eb8ec9ed2db1b68d86103f2ba
Igor Laur <igor.laur2@gmail.com>
2018-12-30 00:41:38 -0200

    Teste conexão homolog e dev

--- sistema/conexao/conexao.php
+++ sistema/conexao/conexao.php
@@ -1,16 +1,25 @@
 <?php 
-    // Passo 1 - Abrir conexão
-    $servidor = "localhost";
-    $usuario = "u431676248_root"; 
-    //$usuario = "root";
-    $senha = "polaco929";
-    //$senha = "root";
-    $banco = "u431676248_andes";
-    //$banco = "andes";
-    $conecta = mysqli_connect($servidor,$usuario,$senha,$banco);
-
-    // Passo 2 - Testar conexão
+    switch($ambiente):
+        case 'root':
+            $servidor = "localhost";
+            $usuario  = "root";
+            $senha    = "root";
+            $banco    = "andes";
+            $conecta = mysqli_connect($servidor,$usuario,$senha,$banco);
+        break;
+        case 'u431676248_root'
+            $servidor = "localhost";
+            $usuario  = "u431676248_root";
+            $senha    = "polaco929";
+            $banco    = "u431676248_andes";
+            $conecta = mysqli_connect($servidor,$usuario,$senha,$banco);
+        break;
+    
+        
+    endswitch;
     if (mysqli_connect_errno()) {// erro e no de numero
         die("Conexão falhou: " + mysqli_connect_errno()); // Se deu erro vou mandar matar a conexão
     }
+
+
 ?>
