0a28cb7c2fa331906a644de37c90adc6f84e3e4f
laur <igor.laur2@gmail.com>
2019-02-08 17:40:32 -0200

    Automatizando o processo do cardápio

---
+++ logica/aluguel.php
@@ -0,0 +1,46 @@
+<?php 
+    $emBreve = '(EM BREVE)';
+    $reservado = '(Reservado)';
+    $indisponivel = '(Indisponível)';
+    $disponível = '(Disponível)';
+?>
+<style>
+    a:link { 
+    text-decoration:none; 
+    } 
+
+    #btn{
+        color: orange;
+    }
+    #verde{
+        background-color: orange;
+        color: white;
+    }
+    #kitnetpequena div{
+        /*color: rgb(43, 255, 0); */    /* Verde */
+        /*color: rgb(254, 137, 0);*/    /* Laranja */
+        color: #ff0000;                 /* Vermelho */
+        /* color: #ffb329; */           /* Laranja */ 
+    }
+    #kitnetmedia div{
+        /* color: rgb(43, 255, 0); */    /* Verde */
+        color: rgb(254, 137, 0);    /* Laranja */
+        /*color: #ff0000;                 /* Vermelho */
+        /* color: #ffb329; */           /* Laranja */ 
+    }
+    #kitnetgrande div{
+        color: rgb(43, 255, 0); */    /* Verde */
+        /*color: rgb(254, 137, 0);*/    /* Laranja */
+        /*color: #ff0000;                 /* Vermelho */
+        /* color: #ffb329; */           /* Laranja */ 
+    }
+    #kitnetluxo div{
+        /*color: rgb(43, 255, 0); */    /* Verde */
+        color: rgb(254, 137, 0);*/    /* Laranja */
+        /*color: #ff0000;                 /* Vermelho */
+        /* color: #ffb329; */           /* Laranja */ 
+    }
+    #airbnb{
+        color: blue;
+    }
+</style>
