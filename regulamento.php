6bc3004232ba57c24e5df05db4f2f252833d2f8b
Igor Laur <igor.laur2@gmail.com>
2020-07-31 16:59:17 -0300

    Inserido o regulamento no site

---
+++ regulamento.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+    <meta name="description" content="Laur's Kitnets - Regulamento">
+    <meta property="og:image" content="www.laur.com.br/regulamento1.jpg">
+    
+    <div id="contact-us" class="contact-us small-11 large-12 columns no-padding small-centered">    
+        <img src="img/regulamento/regulamento1.jpg" title="Regulamento" > <hr/>
+        <img src="img/regulamento/regulamento2.jpg" title="Regulamento" >
+    </div>
+
+<?php include 'footer.php'; ?>
