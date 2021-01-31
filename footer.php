b50372e96b9cfe58ca870f711225288bf14de19f
Igor Laur <igor.laur2@gmail.com>
2021-01-31 03:20:58 -0300

    Correção de links das redes sociais

--- footer.php
+++ footer.php
@@ -13,8 +13,8 @@
 
                 <div class="follow-us small-5 medium-3 small-offset-1 medium-offset-0 columns">
                     <h4 class="footer-section-title">Siga-nos</h4>
-                    <a href="https://www.facebook.com/kitnetcorifeu/?ref=bookmarks" target="blank" title="Facebook"><img src="img/social-icons/facebook.svg" alt="facebook-icon"></a>
-                    <a href="https://www.instagram.com/p/BlgxAF7h9Cj/?taken-by=kitnetusp" target="blank" title="Instagram"><img src="img/social-icons/instagram-backup.svg" alt="facebook-icon"></a>
+                    <a href="https://www.facebook.com/laurkitnets" target="blank" title="Facebook"><img src="img/social-icons/facebook.svg" alt="facebook-icon"></a>
+                    <a href="https://www.instagram.com/laur.com.br" target="blank" title="Instagram"><img src="img/social-icons/instagram-backup.svg" alt="facebook-icon"></a>
                 </div>
                 
                 <div class="contato small-5 medium-3 small-offset-1 medium-offset-0 columns">
