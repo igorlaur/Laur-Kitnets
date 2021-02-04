4c0721e24415eb5766659f94432056f4ff1cdd7f
Igor Laur <igor.laur2@gmail.com>
2021-02-04 13:15:32 -0300

    Hotjar configurado

--- header-blog.php
+++ header-blog.php
@@ -48,6 +48,36 @@
         <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
         <?php include 'shortcut.html'; ?>
         <script src="js/vendor/modernizr.js"></script>
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125555309-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-125555309-1');
+        </script>
+        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="css/foundation.css" />
+        <link rel="stylesheet" href="css/slick.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@569;600;800&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+        <?php include 'shortcut.html'; ?>
+        <script src="js/vendor/modernizr.js"></script>
+            <!-- Hotjar Tracking Code for https://laur.com.br -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2231364,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
     </head>
     
     <body>
--- header-home-office.php
+++ header-home-office.php
@@ -51,6 +51,17 @@
         <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
         <?php include 'shortcut.html'; ?>
         <script src="js/vendor/modernizr.js"></script>
+            <!-- Hotjar Tracking Code for https://laur.com.br -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2231364,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
     </head>
     
     <body>
