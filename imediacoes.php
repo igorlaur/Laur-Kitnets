f6518c703ee3c7b6047b9b69817fb4c4df9db580
Igor Laur <igor.laur2@gmail.com>
2021-02-06 19:10:32 -0300

    Mapa adicionado em imediações

--- imediacoes.php
+++ imediacoes.php
@@ -10,6 +10,8 @@
                 <hr>
             </div>
 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1828.4357453764858!2d-46.72499194181329!3d-23.573058496169317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce565b8b6f7b8d%3A0x10afc06d6515309d!2sLaur&#39;s%20Kitnets%20Butant%C3%A3%20Corifeu%20USP!5e0!3m2!1spt-BR!2sbr!4v1612649191982!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
             <div class="category-slider small-12 columns no-padding">
                 <h4>Universidades</h4>
 
