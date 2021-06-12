4bf07b9831753b5e5c633f6753a40d75f59e33eb
Igor Laur <igor.laur2@gmail.com>
2021-06-12 06:09:01 -0300

    <article> inserido

--- consultoria-kitnets.php
+++ consultoria-kitnets.php
@@ -21,65 +21,67 @@
         }(document, 'script', 'facebook-jssdk'));
     </script>
     <!-- /Facebook SDK -->
-    <div class="about-us small-11 large-12 columns no-padding small-centered" id="about-us">
-        <div class="global-page-container">
-            <div id="about-us-text" class="about-us-title small-12 columns no-padding">
-                <h1>Pretende construir kitnets? Prestamos consultoria!</h1>
-                <hr>
-            </div>
-        
-            <img src="img/logo/laurKitnets.png" alt="Fachada de laur's kitnets" title="Logo de Laur's Kitnets: Aluguel de kitnets">
-            <div class="about-us-text">
-                <p>
-                    Kitnets são um excelente meio de renda passiva para que você possa investir. Saiba como:
-                </p>
-                
-                <h2>
-                    Sobre a consultoria
-                </h2>
-                <img src="blog/consultoria/consultoria-kitnets.jpg" alt="O que é uma kitnet" title="Foto de Kitnet">
-                <p id="referencia">
-                    Fonte da imagem: <a href="https://pixabay.com/pt/photos/escrit%C3%B3rio-neg%C3%B3cios-homens-1209640/">Pixabay</a>
-                    
-                </p>
-                <div
-                    class="fb-like"
-                    data-share="true"
-                    data-width="350"
-                    data-show-faces="true">
+    <article>
+        <div class="about-us small-11 large-12 columns no-padding small-centered" id="about-us">
+            <div class="global-page-container">
+                <div id="about-us-text" class="about-us-title small-12 columns no-padding">
+                    <h1>Pretende construir kitnets? Prestamos consultoria!</h1>
+                    <hr>
                 </div>
-                </br> </br>
-                <p> 
-                        Olá, investidor(a)! Temos experiência com construções e compra de imóveis voltadas para aluguel, como kitnets, studios e 
-                    repúblicas desde 1984 em São Paulo. Começamos com imóveis no edifício Copan, Praça Roosvelt e Nestor Pestana. 
-                    Após alguns anos fundamos a Laur's Kitnets, em 2007, no qual atualmente trabalhamos com kitnets na região do Butantã.
-                </p>
-                <p>
-                    Conosco você terá uma consultoria especializada em kitnets e studios, nela você verá:
-                </p>
             
-                <ol>
-                    <li>A trajetória de Laur's Kitnets;</li>
-                    <li>As melhores localizações para investir em São Paulo;</li>
-                    <li>Construção & infraestrutura</li>
-                        <ul>- Modelos de construção;</ul>
-                        <ul>- Adaptação para o home office;</ul>
-                        <ul>- Tecnologias utilizadas.</ul>
-                    <li>Método de locação;</li>
-                        <ul>- O inquilino ideal;</ul>
-                        <ul>- Sistema de locação;</ul>
-                        <ul>- Regulamento com o decorrer de mais de 35 anos de experiência.</ul>
-                    <li>Modos de divulgação;</li>
-                        <ul>- Métodos de divulgação;</ul>
-                        <ul>- Desenvolvimento de materiais para divulgação;</ul>
-                        <ul>- Plataformas para divulgação;</ul>
-                    <li>Qualidade de vida aos inquilinos;</li>
-                    <li>Retorno financeiro.</li>
-                </ol>
+                <img src="img/logo/laurKitnets.png" alt="Fachada de laur's kitnets" title="Logo de Laur's Kitnets: Aluguel de kitnets">
+                <div class="about-us-text">
+                    <p>
+                        Kitnets são um excelente meio de renda passiva para que você possa investir. Saiba como:
+                    </p>
+                    
+                    <h2>
+                        Sobre a consultoria
+                    </h2>
+                    <img src="blog/consultoria/consultoria-kitnets.jpg" alt="O que é uma kitnet" title="Foto de Kitnet">
+                    <p id="referencia">
+                        Fonte da imagem: <a href="https://pixabay.com/pt/photos/escrit%C3%B3rio-neg%C3%B3cios-homens-1209640/">Pixabay</a>
+                        
+                    </p>
+                    <div
+                        class="fb-like"
+                        data-share="true"
+                        data-width="350"
+                        data-show-faces="true">
+                    </div>
+                    </br> </br>
+                    <p> 
+                            Olá, investidor(a)! Temos experiência com construções e compra de imóveis voltadas para aluguel, como kitnets, studios e 
+                        repúblicas desde 1984 em São Paulo. Começamos com imóveis no edifício Copan, Praça Roosvelt e Nestor Pestana. 
+                        Após alguns anos fundamos a Laur's Kitnets, em 2007, no qual atualmente trabalhamos com kitnets na região do Butantã.
+                    </p>
+                    <p>
+                        Conosco você terá uma consultoria especializada em kitnets e studios, nela você verá:
+                    </p>
+                
+                    <ol>
+                        <li>A trajetória de Laur's Kitnets;</li>
+                        <li>As melhores localizações para investir em São Paulo;</li>
+                        <li>Construção & infraestrutura</li>
+                            <ul>- Modelos de construção;</ul>
+                            <ul>- Adaptação para o home office;</ul>
+                            <ul>- Tecnologias utilizadas.</ul>
+                        <li>Método de locação;</li>
+                            <ul>- O inquilino ideal;</ul>
+                            <ul>- Sistema de locação;</ul>
+                            <ul>- Regulamento com o decorrer de mais de 35 anos de experiência.</ul>
+                        <li>Modos de divulgação;</li>
+                            <ul>- Métodos de divulgação;</ul>
+                            <ul>- Desenvolvimento de materiais para divulgação;</ul>
+                            <ul>- Plataformas para divulgação;</ul>
+                        <li>Qualidade de vida aos inquilinos;</li>
+                        <li>Retorno financeiro.</li>
+                    </ol>
+                </div>
             </div>
+            <?php include "botao-consultoria.php"; ?>
         </div>
-        <?php include "botao-consultoria.php"; ?>
-    </div>
+    </article>
 </body>
 <style>
     #btn:hover{
